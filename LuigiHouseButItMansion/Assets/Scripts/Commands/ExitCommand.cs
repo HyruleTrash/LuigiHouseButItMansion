4a21f101a0696f538bc9a33b8e89089ebf1dafa9
“HyruleTrash” <83176601+HyruleTrash@users.noreply.github.com>
2025-12-17 12:26:31 +0100

    update: reorganized some folders and added escape shortcut

---
+++ LuigiHouseButItMansion/Assets/Scripts/Commands/ExitCommand.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ExitCommand : ICommand
+{
+    public void Execute()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ LuigiHouseButItMansion/Assets/Scripts/Commands/GlobalControls.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GlobalControls : SingletonBehaviour<GlobalControls>
+{
+    [SerializeField]
+    private InputActionAsset inputActionAsset;
+    private InputAction exitGameAction;
+    
+    private void OnEnable()
+    {
+        inputActionAsset.FindActionMap("GlobalControls").Enable();
+    }
+    
+    private void OnDisable()
+    {
+        inputActionAsset.FindActionMap("GlobalControls").Disable();
+    }
+
+    private void Start()
+    {
+        exitGameAction = InputSystem.actions.FindAction("Exit");
+        exitGameAction.started += context =>
+        {
+            new ExitCommand().Execute();
+        };
+    }
+}
