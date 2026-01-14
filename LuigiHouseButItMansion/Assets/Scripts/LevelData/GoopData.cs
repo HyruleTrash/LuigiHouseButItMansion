20759fdfd9f7f62d1b3fa5757415653990796b42
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2026-01-14 16:12:24 +0100

    Added: Custom Lit surface, that adds a GOOP mask, unique per room

---
+++ LuigiHouseButItMansion/Assets/Scripts/LevelData/GoopData.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoopData", menuName = "ScriptableObjects/RoomData/Goop")]
+public class GoopData : ScriptableObject
+{
+    public Color goopColor;
+    public float cutoffThreshold = 0.1f;
+}
--- LuigiHouseButItMansion/Assets/Scripts/LevelData/RoomObjectData.cs
+++ LuigiHouseButItMansion/Assets/Scripts/LevelData/RoomObjectData.cs
@@ -1,20 +1,45 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Serialization;
 
 public class RoomObjectData : MonoBehaviour
 {
+    private static readonly int GoopMask = Shader.PropertyToID("_GoopMask");
+    private static readonly int GoopColor = Shader.PropertyToID("_GoopColor");
+    private static readonly int RoomMin = Shader.PropertyToID("_RoomMin");
+    private static readonly int RoomSize = Shader.PropertyToID("_RoomSize");
+    private static readonly int CutOffThreshold = Shader.PropertyToID("_CutOffThreshold");
+    private static Vector3 setRoomMin;
+    private static Vector3 setRoomSize;
+    private static Color setRoomColor;
+    private static Texture2D setRoomGoopTex;
+    private static float setRoomCutOffThreshold;
+
     private List<RoomEntrance> entrances;
     public Vector3 cameraViewPoint;
     public RoomCameraConfig cameraConfig;
     public Action OnReadyRoom;
     public InteractableObjectsManager interactableObjectsManager;
+
+    [SerializeField] private bool shouldResetTex = false;
+    [SerializeField]
+    private Texture2D roomGoopTex = null;
     
+    [SerializeField] 
+    private GoopData goopData;
+    [SerializeField]
+    private Bounds roomBounds;
+
+
     private void Start()
     {
         RoomManager.instance.LiveRooms.Add(this);
+        setRoomGoopTex = null;
     }
 
     private void OnDestroy()
@@ -29,8 +54,24 @@ public class RoomObjectData : MonoBehaviour
         var startPosition = transform.position;
         var endPosition = cameraViewPoint * 2;
         Gizmos.DrawLine(startPosition + offset, startPosition + endPosition + offset);
+
+        SetGlobalShaderGoopData();
+        Gizmos.DrawWireCube(roomBounds.center + transform.position, roomBounds.size);
     }
-    
+
+    private void OnValidate()
+    {
+        if (!Application.isPlaying)
+        {
+            if (shouldResetTex)
+            {
+                roomGoopTex = null;
+                shouldResetTex = false;
+            }
+            SetGlobalShaderGoopData();
+        }
+    }
+
     public void AddEntrance(RoomEntrance entrance)
     {
         entrances ??= new List<RoomEntrance>();
@@ -47,6 +88,45 @@ public class RoomObjectData : MonoBehaviour
 
         UnLockDoors();
         OnReadyRoom?.Invoke();
+
+        SetGlobalShaderGoopData();
+    }
+
+    public void SetGlobalShaderGoopData()
+    {
+        if (goopData == null)
+        {
+            setRoomGoopTex = null;
+        }
+        else
+        {
+            if (roomGoopTex == null)
+            {
+                var format = GraphicsFormat.R8_UNorm;
+                roomGoopTex = new Texture2D(512, 512, format, TextureCreationFlags.None);
+                
+                byte[] rawData = new byte[512 * 512];
+                for (int i = 0; i < rawData.Length; i++)
+                {
+                    rawData[i] = (byte)(i * 255 / (512 * 512 - 1));  // Continuous progression
+                }
+                
+                roomGoopTex.SetPixelData(rawData, 0);
+                roomGoopTex.Apply();
+            }
+
+            setRoomGoopTex = roomGoopTex;
+            setRoomMin = roomBounds.min + transform.position;
+            setRoomSize = roomBounds.size;
+            setRoomColor = goopData.goopColor;
+            setRoomCutOffThreshold = goopData.cutoffThreshold;
+        }
+
+        Shader.SetGlobalTexture(GoopMask, setRoomGoopTex);
+        Shader.SetGlobalVector(RoomMin, setRoomMin);
+        Shader.SetGlobalVector(RoomSize, setRoomSize);
+        Shader.SetGlobalColor(GoopColor, setRoomColor);
+        Shader.SetGlobalFloat(CutOffThreshold, setRoomCutOffThreshold);
     }
 
     public void DisableRoom()
